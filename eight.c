070ce0145ad69d3ec1c60cd358a99d60aec15f41
Berrouk-Salma <salmaberrouk4@gmail.com>
2024-09-25 08:11:04 +0100

    add file

---
+++ eight.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+ int nombre_1 , nombre_2 ,nombre_3 ,moyenne_geometrique; 
+  printf("saiser le nombre 1: ");
+  scanf("%d",&nombre_1 );
+  printf("saiser le nombre 2 : ");
+  scanf("%d",&nombre_2 );
+  printf("saiser le nombre 3 : ");
+  scanf("%d",&nombre_3 );
+
+
+
+moyenne_geometrique = pow((nombre_1 * nombre_2 * nombre_3),1/3);
+
+printf("la moyenne geometrique est : %d ",moyenne_geometrique);
+
+  return 0;
+}
